05277c0f7ce75be159c770ac1989689c41f8143a
172194177 <pete172194177@gmail.com>
2018-11-21 19:08:01 +0300

    ..

--- Conv2D.go
+++ Conv2D.go
@@ -39,9 +39,7 @@ func (t1 *Tensor) Get(x, y int) float64 {
 }
 
 func (t1 *Tensor) Set(x, y int, val float64) {
-	(*t1).Data[y*(*t1).Size.X+x] = val //Set(i, j, data[j*c+i])
-	//fmt.Println((*t1).Data[y*(*t1).Size.X+x])
-	//fmt.Println(y,x)
+	(*t1).Data[y*(*t1).Size.X+x] = val
 }
 
 // SetData - Set data for *Tensor
@@ -80,41 +78,44 @@ outputY := ((*t1).Size.Y - t2.Size.Y + 2*padding[1])/stride[1] + 1
 // fmt.Println(outputX, outputY)
 outputData := NewTensor(outputX, outputY)
 
-// find center position of kernel (half of kernel size)
-kCenterX := t2.Size.X / 2;
-kCenterY := t2.Size.Y / 2;
 
-// index to input element into output array
+// index for input element in output array
 el := 0
 
 
-	for i := 0; i < outputX; i = (i + stride[0]) {              // rows
-	    for j := 0; j < outputY; j = (j + stride[1]) {          // columns
-	    	if i == 0 && j == 0 {
-	    		el = 0
-	    	} else {
-	    		el += 1
-	    	}
-	        for m := 0; m < t2.Size.Y; m++ {     // kernel rows
-	            mm := t2.Size.Y - 1 - m;      	// row index of flipped kernel
-	            for n := 0; n < t2.Size.X; n++ {	// kernel columns
+	for i := 0; i < outputX; i = (i + stride[0]) {				// rows
+		for j := 0; j < outputY; j = (j + stride[1]) {			// columns
+			if i == 0 && j == 0 {
+				el = 0
+			} else {
+				el += 1
+			}
+			for m := 0; m < t2.Size.X; m++ {		// kernel rows
+				for n := 0; n <	t2.Size.Y; n++ {	// kernel columns
 
-	                nn := t2.Size.X - 1 - n;  // column index of flipped kernel
-
-	                // index of input signal, used for checking boundary
-	                ii := i + (kCenterY - mm);
-	                jj := j + (kCenterX - nn);
+					// index of input signal, used for checking boundary
+					ii := i + m;
+					jj := j + n;
 
 	                // ignore input samples which are out of bound
-	                if ii >= 0 && ii < (*t1).Size.Y && jj >= 0 && jj < (*t1).Size.X {
-	                	outputElement := (*t1).Data[ii + jj] * t2.Data[mm + nn]
-	                    outputData.Data[el] += outputElement
-	                    // fmt.Println(i, j, el, outputData.Data[el])
+					if ii >= 0 && ii < (*t1).Size.X && jj >= 0 && jj < (*t1).Size.Y {
+						outputElement := 0.0
+						kernelElement := t2.Data[(m * t2.Size.X + n)]
+						if i == 0 {
+							inputElement := (*t1).Data[( (m) * t1.Size.X + n) + j]
+							outputElement = inputElement * kernelElement
+						} else {
+							inputElement := (*t1).Data[ii * (*t1).Size.X + jj]
+							outputElement =  inputElement * kernelElement
+						}
+						// fmt.Println("ii,jj: ", ii, jj, "||", (m * t2.Size.X + n), "||", (*t1).Data[ii * (*t1).Size.X + jj], "*", t2.Data[(m * t2.Size.X + n)], "outputElement: ", outputElement)
+
+						// Filling output array
+						outputData.Data[el] += outputElement
 					}
-	            }
-	        }
-	        fmt.Println(i, j, el, outputData.Data[el])
-	    }
+				}
+			}
+		}
 	}
 	fmt.Print("Input Data: ", t1, "\n", "Kernel: ", t2, "\n", "Output Data: ", outputData)
 	return outputData
@@ -124,14 +125,11 @@ el := 0
 func main() {
 	inputData := NewTensor(7, 7)
 	inputData.SetData(7, 7, []float64{1, 5, 6, 4, 5, 4, 4, 6, 1, 4, 4, 4, 2, 7, 2, 3, 9, 3, 4, 4, 4, 9, 8, 4, 6, 3, 2, 4, 6, 4, 3, 3, 2, 1, 3, 1, 1, 1, 1, 1, 1, 1, 2, 2, 3, 4, 2, 5, 4})
-	//fmt.Println(inputData)
-	//inputData.Print()
 
 	kernel := NewTensor(3, 3)
 	kernel.SetData(3, 3, []float64{1, 1, 1, 2, 2, 2, 3, 3, 3})
-	//fmt.Println(kernel)
-	//inputData.Print()
-	stride := [2]int{2, 2}
+
+	stride := [2]int{1, 1}
 	padding := [2]int{0, 0}
 	inputData.Conv2D(kernel, stride, padding)
 }
