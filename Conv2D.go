c52c461c473418fb0a9fbdff6f40a97dd027f1a0
172194177 <pete172194177@gmail.com>
2018-11-29 17:43:57 +0300

    .

--- Conv2D.go
+++ Conv2D.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 // Point - dimensions
@@ -71,75 +72,133 @@ func (t1 *Tensor) Print() {
 
 
 // Conv2D - apply convolution to t1 using t2 kernel
-func (t1 *Tensor) Conv2D (t2 Tensor, stride [2]int, padding [2]int) Tensor {
+func (t1 *Tensor) Conv2D (t2 Tensor, stride [2]int, padding [2]int, attribute bool) Tensor {
 
-outputX := ((*t1).Size.X - t2.Size.X + 2*padding[1])/stride[1] + 1
-outputY := ((*t1).Size.Y - t2.Size.Y + 2*padding[0])/stride[0] + 1
-// fmt.Println(outputX, outputY)
-outputData := NewTensor(outputX, outputY)
+	var outputX int
+	var outputY int
+	var paddingedData []float64
+	verticalPaddingArr := make([]float64, padding[0] * t1.Size.Y + padding[1] * 2)
+	horizontalPaddingArr := make([]float64, padding[1])
 
+	if attribute == true {
+		outputX = ( (*t1).Size.X - t2.Size.X + 2*padding[1])/stride[1] + 1
+		outputY = ( (*t1).Size.Y - t2.Size.Y + 2*padding[0])/stride[0] + 1
+	} else {
+		outputX = ( (*t1).Size.X - t2.Size.X)/stride[1] + 1
+		outputY = ( (*t1).Size.Y - t2.Size.Y)/stride[0] + 1
+	}
 
-// index for input element in output array
-el := 0
-iLimit := outputY
-jLimit := outputX
-if stride[0] > 1 {
-	iLimit = outputY + stride[0] + 1
-}
-if stride[1] > 1 {
-	jLimit = outputX + stride[1] + 1
-}
+	paddingedData = t1.Data
+	fmt.Println("original arr sizes:", (*t1).Size.X, "x", (*t1).Size.Y, "\nlen:", len(t1.Data), len(paddingedData) )
 
+	// for i := 0; i < (*t1).Size.X; i++{
+	// 	paddingedData = append(paddingedData[:1], 0.0)
+	// 	for j := 0; j < (*t1).Size.Y; j++{
+	// 		fmt.Print(j, ", ")
+	// 	}
+	// }
 
-	for i := 0; i < iLimit; i = (i + stride[0]) {				// rows
-		for j := 0; j < jLimit; j = (j + stride[1]) {			// columns
-			if i == 0 && j == 0 {
-				el = 0
-			} else {
-				el += 1
+	for i := 0; i < len(paddingedData); i++ {
+			for j := 0; j < 2; j ++ {
+		    	if j == 0 {
+					fmt.Println("j =", j, "len =", len((paddingedData)[ (i * t1.Size.X + j) : ]) )
+					paddingedData = append(horizontalPaddingArr, (paddingedData)[ (i * t1.Size.X) : ]...)
+					fmt.Println(paddingedData)
+					// inputElement := (*t1).Data[ii * (*t1).Size.X + jj]
+					// break
+				}
+		    	if j ==  1 {
+					// (paddingedData) = append([]float64{t1.Data[0]}, append((paddingedData)[:p], (paddingedData)[p+1:]...)...)
+					fmt.Println("j =", j, "len =", len((paddingedData)[:j+1]) )
+					// paddingedData = append(paddingedData, horizontalPaddingArr, append((paddingedData)[:p], (paddingedData)[p+1:]...)...)
+					paddingedData = append((paddingedData)[ (i * t1.Size.X) : (i * t1.Size.X + t1.Size.Y)], append(horizontalPaddingArr, (paddingedData)[ (i * t1.Size.X + 1) : ]...)...)
+					fmt.Println(paddingedData)
+				}
 			}
-			for m := 0; m < t2.Size.X; m++ {		// kernel rows
-				
-				for n := 0; n <	t2.Size.Y; n++ {	// kernel columns
+		os.Exit(3)	
+    	if i == 0 {
+			fmt.Println("i =", i, "len =", len((paddingedData)[:i+1]) )
+			paddingedData = append(verticalPaddingArr, paddingedData...)
+			// break
+		}
+    	if i == (len(t1.Data) - 1) {
+			// (paddingedData) = append([]float64{t1.Data[0]}, append((paddingedData)[:p], (paddingedData)[p+1:]...)...)
+			fmt.Println("i =", i, "len =", len((paddingedData)[:i+1]) )
+			// paddingedData = append(paddingedData, verticalPaddingArr, append((paddingedData)[:p], (paddingedData)[p+1:]...)...)
+			paddingedData = append(paddingedData, verticalPaddingArr...)
+		}
+	}
+	fmt.Println(paddingedData)
+	fmt.Println("paddinged len:", len(paddingedData), "\nverticalPaddingArr:", len(verticalPaddingArr), "\npaddinged len - verticalPaddingArr len =", (len(paddingedData) - 2*len(verticalPaddingArr) ) )
 
-					// index of input signal, used for checking boundary
-					ii := i + m;
-					jj := j + n;
+	os.Exit(3)
+	outputData := NewTensor(outputX, outputY)
 
-	                // ignore input samples which are out of bound
-					if ii >= 0 && ii < (*t1).Size.Y && jj >= 0 && jj < (*t1).Size.X {
-						outputElement := 0.0
-						kernelElement := t2.Data[(m * t2.Size.X + n)]
-						if i == 0 {
-							inputElement := (*t1).Data[( (m) * t1.Size.X + n) + j]
-							outputElement = inputElement * kernelElement
-						} else {
-							inputElement := (*t1).Data[ii * (*t1).Size.X + jj]
-							outputElement =  inputElement * kernelElement
+
+	// index for input element in output array
+	el := 0
+	iLimit := outputY
+	jLimit := outputX
+	if stride[0] > 1 {
+		iLimit = outputY + stride[0] + 1
+	}
+	if stride[1] > 1 {
+		jLimit = outputX + stride[1] + 1
+	}
+
+		for i := 0; i < iLimit; i = (i + stride[0]) {				// rows
+			for j := 0; j < jLimit; j = (j + stride[1]) {			// columns
+				if i == 0 && j == 0 {
+					el = 0
+				} else {
+					el += 1
+				}
+				for m := 0; m < t2.Size.X; m++ {		// kernel rows
+					
+					for n := 0; n <	t2.Size.Y; n++ {	// kernel columns
+
+						// index of input signal, used for checking boundary
+						ii := i + m;
+						jj := j + n;
+
+		                // ignore input samples which are out of bound
+						if ii >= 0 && ii < (*t1).Size.Y && jj >= 0 && jj < (*t1).Size.X {
+							outputElement := 0.0
+							kernelElement := t2.Data[(m * t2.Size.X + n)]
+							if i == 0 {
+								inputElement := (*t1).Data[( (m) * t1.Size.X + n) + j]
+								outputElement = inputElement * kernelElement
+							} else {
+								inputElement := (*t1).Data[ii * (*t1).Size.X + jj]
+								outputElement =  inputElement * kernelElement
+							}
+							fmt.Println("i,j", i, j, "\tii,jj: ", ii, jj, "\t||", (m * t2.Size.X + n), "||\t", (*t1).Data[ii * (*t1).Size.X + jj], "*", t2.Data[(m * t2.Size.X + n)])
+
+							// Filling output array
+							outputData.Data[el] += outputElement
 						}
-						// fmt.Println("i,j", i, j, "\tii,jj: ", ii, jj, "\t||", (m * t2.Size.X + n), "||\t", (*t1).Data[ii * (*t1).Size.X + jj], "*", t2.Data[(m * t2.Size.X + n)])
-
-						// Filling output array
-						outputData.Data[el] += outputElement
 					}
 				}
 			}
 		}
-	}
-	fmt.Print("\nInput Data: ", t1, "\n\n", "Kernel: ", t2, "\n\n", "Output Data", outputData.Size, " :\n\n")
-	return outputData
+		fmt.Print("\nInput Data: ", t1, "\n\n", "Kernel: ", t2, "\n\n", "Output Data", outputData.Size, " :\n\n")
+		return outputData
 }
 
 
 func main() {
 	inputData := NewTensor(8, 9)
 	inputData.SetData(8, 9, []float64{-0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, -0.9, -0.10, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, -0.17, 0.18, -0.19, 0.20, 0.21, 0.22, 0.23, 0.24, -0.25, 0.26, 0.27, -0.28, 0.29, 0.30, 0.31, 0.32, -0.33, 0.34, 0.35, 0.36, -0.37, 0.38, 0.39, 0.40, -0.41, 0.42, 0.43, 0.44, 0.45, -0.46, 0.47, 0.48, -0.49, 0.50, 0.51, 0.52, 0.53, 0.54, -0.55, 0.56, -0.57, 0.58, 0.59, 0.60, 0.61, 0.62, 0.63, -0.64, -0.65, 0.66, 0.67, 0.68, 0.69, 0.70, 0.71, 0.72})
+	// inputData := NewTensor(32, 32)
+	// inputData.SetData(32, 32, []float64{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
 
 	kernel := NewTensor(3, 3)
 	kernel.SetData(3, 3, []float64{0.10466029, -0.06228581, -0.43436298, 0.44050909, -0.07536250, -0.34348075, 0.16456005, 0.18682307, -0.40303048})
+	// kernel := NewTensor(5, 5)
+	// kernel.SetData(5, 5, []float64{0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0})
 
-	stride := [2]int{4, 1}
-	padding := [2]int{0, 0}
-	res := inputData.Conv2D(kernel, stride, padding)
+	stride := [2]int{1, 1}
+	padding := [2]int{2, 2}
+	res := inputData.Conv2D(kernel, stride, padding, true)
 	res.Print()
 }
